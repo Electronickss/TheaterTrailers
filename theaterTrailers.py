d3172c5671ee8438bcac83b882717f9ea25e15cb
Vuokko Vuorinnen <VuokkoVuorinnen@users.noreply.github.com>
2019-02-15 14:56:53 +0100

    Update theaterTrailers.py
    Trailers only downloaded in 720p for me, with this fix we ask youtubedl to download the bestvideo (in most cases 1080p) for the trailers.

--- theaterTrailers.py
+++ theaterTrailers.py
@@ -298,7 +298,7 @@ def videoDownloader(string, passedTitle, yearVar):
   ydl1_opts = {
     'outtmpl': os.path.join(TheaterTrailersHome, 'Trailers', '{0} ({1})'.format(passedTitle, yearVar), '{0} ({1}).mp4'.format(passedTitle, yearVar)),
     'ignoreerrors': True,
-    'format': 'mp4',
+    'format': 'bestvideo[ext=mp4]',
   }
   with youtube_dl.YoutubeDL(ydl1_opts) as ydl:
     logger.info("downloading {0} from {1}".format(passedTitle, string))
